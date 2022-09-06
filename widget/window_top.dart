69d03d2f39032e3285fbebeb1461cb59c48a9f13
18598925736 <545679452@qq.com>
2022-09-06 16:04:58 +0800

    Update window_top.dart

--- widget/window_top.dart
+++ widget/window_top.dart
@@ -1,1 +1,45 @@
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color _mainColor = Colors.blueGrey;
+
+/// 顶部操作按钮
+class WindowTopBox extends StatelessWidget {
+  const WindowTopBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget current;
+
+    current = WindowTitleBarBox(
+        child: Row(children: [
+      Expanded(
+          child: Container(
+              color: _mainColor,
+              child: MoveWindow(
+                  child: Row(children: const [
+                SizedBox(width: 20),
+                Text(
+                  'Json解析工具',
+                  style: TextStyle(fontWeight: FontWeight.w700, color: Colors.white),
+                )
+              ])))),
+      _WindowButtons()
+    ]));
+
+    return current;
+  }
+}
+
+class _WindowButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      MinimizeWindowButton(),
+      MaximizeWindowButton(),
+      CloseWindowButton(),
+    ]);
+  }
+}
